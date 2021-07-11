e5fe90cdaf3f1f5409936e398280b7a70f663566
Bernat Gómez <bernat.gomez@gmail.com>
2021-07-11 19:18:21 +0200

    Modify criteria factory

--- lib/common/models/OrderCriteriaModel.dart
+++ lib/common/models/OrderCriteriaModel.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import 'core/BaseModel.dart';
 
 
@@ -5,9 +7,15 @@ import 'core/BaseModel.dart';
  * Factory that provides ordering criteria objects
  */
 abstract class OrderCriteriaModelFactory {
-  static OrderCriteriaModel priceAscending() => OrderCriteriaModel.priceAscending();
 
-  static OrderCriteriaModel priceDescending() => OrderCriteriaModel.priceDescending();
+  static List<OrderCriteriaModel> allCriterias({@required String lbPriceAsc, @required String lbPriceDesc}) => [
+    priceAscending(lbPriceAsc),
+    priceDescending(lbPriceDesc)
+  ];
+
+  static OrderCriteriaModel priceAscending(String lb) => OrderCriteriaModel.priceAscending(lb);
+
+  static OrderCriteriaModel priceDescending(String lb) => OrderCriteriaModel.priceDescending(lb);
 }
 
 /*
@@ -24,12 +32,24 @@ class OrderCriteriaModel extends BaseModel {
 
   const OrderCriteriaModel._(this.id, this.name, this.reverse) : super();
 
-  factory OrderCriteriaModel.priceAscending() => OrderCriteriaModel._(1, "price", false);
+  factory OrderCriteriaModel.priceAscending(String lb) => OrderCriteriaModel._(1, lb, false);
 
-  factory OrderCriteriaModel.priceDescending() => OrderCriteriaModel._(2, "price", true);
+  factory OrderCriteriaModel.priceDescending(String lb) => OrderCriteriaModel._(2, lb, true);
 
   @override
   bool validate() {
     this.name.isNotEmpty;
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OrderCriteriaModel &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          reverse == other.reverse;
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ reverse.hashCode;
 }
