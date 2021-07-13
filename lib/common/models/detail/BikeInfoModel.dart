ecc3634847d4d1380c71747dbb3e256f73f50eb9
Bernat Gómez <bernat.gomez@gmail.com>
2021-07-13 20:07:16 +0200

    Modify model

--- lib/common/models/detail/BikeInfoModel.dart
+++ lib/common/models/detail/BikeInfoModel.dart
@@ -5,6 +5,8 @@ import '../core/BaseModel.dart';
  * Data model with additional info about a bike
  */
 class BikeInfoModel extends BaseModel {
+  static const NUM_DECIMALS = 2;
+
   final String descrip;
   final double weight;
   final double wheelSize;
@@ -13,10 +15,16 @@ class BikeInfoModel extends BaseModel {
 
   const BikeInfoModel({this.descrip = "", this.weight = 0.0, this.wheelSize = 0.0, this.frontLight = false, this.rearLight = false}) :super();
 
+  factory BikeInfoModel.empty() => BikeInfoModel();
+
   @override
   bool validate() => this.descrip.isNotEmpty;
 
   bool hasFrontLight() => this.frontLight;
 
   bool hasRearLight() => this.rearLight;
+
+  String get formattedWeight => this.weight.toStringAsFixed(NUM_DECIMALS);
+
+  String get formattedWheelSize => this.wheelSize.toStringAsFixed(NUM_DECIMALS);
 }
