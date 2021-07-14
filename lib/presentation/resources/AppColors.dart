c17b7a68f6ae0a451acadb133bae2c5824e1f122
Bernat Gómez <bernat.gomez@gmail.com>
2021-07-14 18:29:52 +0200

    Modify colors

--- lib/presentation/resources/AppColors.dart
+++ lib/presentation/resources/AppColors.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 
 abstract class AppColors {
-  static final Color primary = Colors.red.shade200;
-  static final Color primaryDark = Colors.red.shade600;
-  static const Color accent = Colors.blueAccent;
+  static const Color primary = Color(0xFFD0B3B4);
+  static const Color primaryDark = Color(0xFF582C4D);
+  static const Color accent = Color(0xFFB89451);
+  static const Color accentLight = Color(0xFFffd166);
   static const Color trans = Colors.transparent;
 
   static const Color actionColor = AppColors.white;
